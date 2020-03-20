9acf6f94de504c6e869e131d665e92e8f08addbd
Salah Hasan <ft2059@wayne.edu>
2020-03-19 21:43:49 -0400

    Test comment and updated .gitignorefile

--- src/introduction/HelloJava.java
+++ src/introduction/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		
 		System.out.print("Type your name here");
-		
+		System.out.print("Welcome ");
+		System.out.print("Test comment");
 	}
 }
